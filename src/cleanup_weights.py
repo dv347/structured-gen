19f6a9376cfdd9366cf9cb40e158f882b8dd8e1d
dv347 <dv347@cam.ac.uk>
2025-04-15 14:45:33 +0100

    Update weights cleanup script to match nested format

--- src/cleanup_weights.py
+++ src/cleanup_weights.py
@@ -15,19 +15,22 @@ def cleanup_model_dir():
     for dataset_dir in safe_listdir(MODELS_DIR):
         dataset_dir_path = os.path.join(MODELS_DIR, dataset_dir)
 
-        for model_dir in safe_listdir(dataset_dir_path):
-            model_dir_path = os.path.join(dataset_dir_path, model_dir)
+        for stage_dir in safe_listdir(dataset_dir_path):
+            stage_dir_path = os.path.join(dataset_dir_path, stage_dir)
 
-            for item in safe_listdir(model_dir_path):
-                item_path = os.path.join(model_dir_path, item)
-                if os.path.isdir(item_path):
-                    if item.startswith("checkpoint") or item == "merged_model":
-                        logger.info(f"Deleting directory: {item_path}")
-                        shutil.rmtree(item_path)
+            for model_dir in safe_listdir(stage_dir_path):
+                model_dir_path = os.path.join(stage_dir_path, model_dir)
 
-                elif os.path.isfile(item_path) and os.path.basename(item_path) not in PRESERVE_FILES:
-                    logger.info(f"Deleting file: {item_path}")
-                    os.remove(item_path)
+                for item in safe_listdir(model_dir_path):
+                    item_path = os.path.join(model_dir_path, item)
+                    if os.path.isdir(item_path):
+                        if item.startswith("checkpoint") or item == "merged_model":
+                            logger.info(f"Deleting directory: {item_path}")
+                            shutil.rmtree(item_path)
+
+                    elif os.path.isfile(item_path) and os.path.basename(item_path) not in PRESERVE_FILES:
+                        logger.info(f"Deleting file: {item_path}")
+                        os.remove(item_path)
 
 
 if __name__ == "__main__":
