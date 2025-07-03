c74a2c3729d7f245282540a7c10ff534d1e773d2
dv347 <dv347@cam.ac.uk>
2025-07-03 15:22:35 +0100

    Update accuracy calculation to include cases where the answer is nested

--- src/eval.py
+++ src/eval.py
@@ -6,6 +6,6 @@ from dataset import TestCase
 def evaluate_accuracy(test_cases: List[TestCase]) -> float:
     counter = 0
     for test_case in test_cases:
-        if test_case.target == test_case.prediction:
+        if test_case.target in test_case.prediction:
             counter += 1
     return counter / len(test_cases)
